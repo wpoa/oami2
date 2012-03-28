d43a3923f753272c1abef2f75088eaaa52fe34ee
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2012-03-28 05:48:56 +0200

    + oa-cache “list-articles” action implemeted, returns CSV with article metadata

--- sources/pubmed.py
+++ sources/pubmed.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from os import path
+from datetime import date
+from os import listdir, path
 from urllib2 import urlopen, urlparse
+from xml.etree.cElementTree import dump, ElementTree
+# the C implementation of ElementTree is 5 to 20 times faster than the Python one
+
+import tarfile
 
 # According to <ftp://ftp.ncbi.nlm.nih.gov/README.ftp>, this should be
 # 33554432 (32MiB) for best performance. Note that on slow connections,
@@ -50,3 +55,133 @@ def download_metadata(target_directory):
                     }
                 else:
                     break
+
+def list_articles(target_directory):
+    """
+    Iterates over archive files in target_directory, yielding article information.
+    """
+    listing = listdir(target_directory)
+    for filename in listing:
+        with tarfile.open(path.join(target_directory, filename)) as archive:
+            for item in archive:
+                if path.splitext(item.name)[1] == '.nxml':
+                    content = archive.extractfile(item)
+                    tree = ElementTree()
+                    tree.parse(content)
+
+                    result = {}
+                    result['article-contrib-authors'] = _get_article_contrib_authors(tree)
+                    result['article-title'] = _get_article_title(tree)
+                    result['article-abstract'] = _get_article_abstract(tree)
+                    result['journal-title'] = _get_journal_title(tree)
+                    result['article-date'] = _get_article_date(tree)
+                    result['article-url'] = _get_article_url(tree)
+                    result['article-license-url'] = _get_article_license_url(tree)
+                    result['article-license-holder'] = _get_article_license_holder(tree)
+                    yield result
+
+def _get_article_contrib_authors(tree):
+    from sys import stderr
+    """
+    Given an ElementTree, returns article authors in a format suitable for citation.
+    """
+    authors = []
+    front = ElementTree(tree).find('front')
+    for contrib in front.iter('contrib'):
+        contribTree = ElementTree(contrib)
+        try:
+            surname = contribTree.find('name/surname').text
+        except AttributeError:  # author is not a natural person
+            citation_name = contribTree.find('collab').text
+            if citation_name is not None:
+                authors.append(citation_name)
+            continue
+
+        try:
+            given_names = contribTree.find('name/given-names').text
+            citation_name = ' '.join([surname, given_names[0]])
+        except AttributeError:  # no given names
+            citation_name = surname
+        except TypeError:  # also no given names
+            citation_name = surname
+        if citation_name is not None:
+            authors.append(citation_name)
+
+    return ', '.join(authors)
+
+def _get_article_title(tree):
+    """
+    Given an ElementTree, returns article title.
+    """
+    front = ElementTree(tree).find('front')
+    for title in front.iter('article-title'):
+        return title.text
+
+def _get_article_abstract(tree):
+    # FIXME: this does not work right
+    """
+    Given an ElementTree, returns article abstract.
+    """
+    result = []
+    front = ElementTree(tree).find('front')
+    for abstract in front.iter('abstract'):
+        for element in abstract.iter():
+            if element.text is not None:
+                result.append(element.text)
+    return '  '.join(result)
+
+def _get_journal_title(tree):
+    """
+    Given an ElementTree, returns journal title.
+    """
+    front = ElementTree(tree).find('front')
+    for journal_meta in front.iter('journal-meta'):
+        for journal_title in journal_meta.iter('journal-title'):
+            return journal_title.text
+
+def _get_article_date(tree):
+    """
+    Given an ElementTree, returns article date.
+    """
+    article_meta = ElementTree(tree).find('front/article-meta')
+    for pub_date in article_meta.iter('pub-date'):
+        if pub_date.attrib['pub-type'] == 'epub':
+            pub_date_tree = ElementTree(pub_date)
+            year = int(pub_date_tree.find('year').text)
+            month = int(pub_date_tree.find('month').text)
+            day = int(pub_date_tree.find('day').text)
+            return str(date(year, month, day))
+
+def _get_article_url(tree):
+    """
+    Given an ElementTree, returns article URL.
+    """
+    article_meta = ElementTree(tree).find('front/article-meta')
+    for article_id in article_meta.iter('article-id'):
+        if article_id.attrib['pub-id-type'] == 'doi':
+            return 'http://dx.doi.org/' + article_id.text
+
+def _get_article_license_url(tree):
+    """
+    Given an ElementTree, returns article license URL.
+    """
+    license = ElementTree(tree).find('front/article-meta/permissions/license')
+    try:
+        return license.attrib['{http://www.w3.org/1999/xlink}href']
+    except AttributeError:  # license statement is missing
+        return ''
+    except KeyError:  # license statement is in plain text
+        return ''
+
+def _get_article_license_holder(tree):
+    # FIXME: this should probably be named _get_article_copyright_holder
+    """
+    Given an ElementTree, returns article license holder.
+    """
+    copyright_holder = ElementTree(tree).find(
+        'front/article-meta/permissions/copyright-holder'
+    )
+    try:
+        return copyright_holder.text
+    except AttributeError:  # no copyright_holder known
+        return ''
