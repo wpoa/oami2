76bddb422006f71a3086005d95a091cadb8ecc75
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2012-07-01 12:50:20 +0200

    - remove PMID in upload template, DOI suffices

--- helpers/template.py
+++ helpers/template.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def page(authors, article_title, journal_title, date, article_url, \
-    license_url, rights_holder, label, caption, pmid):
+    license_url, rights_holder, label, caption):
     license_templates = {
         'http://creativecommons.org/licenses/by/2.0': '{{cc-by-2.0}}',
         'http://creativecommons.org/licenses/by-sa/2.0': '{{cc-by-sa-2.0}}',
@@ -18,8 +18,8 @@ def page(authors, article_title, journal_title, date, article_url, \
     text += "{{Information\n"
     text += "|Description=\n{{en|%s}}\n" % caption
     text += "|Date= %s\n" % date
-    text += "|Source= %s from %s, %s. PMID %s. Available from %s\n" % \
-        (label, article_title, journal_title, pmid, article_url)
+    text += "|Source= %s from %s, %s. Available from %s\n" % \
+        (label, article_title, journal_title, article_url)
     text += "|Author= %s\n" % authors
     text += "|Permission= %s Copyright owner: %s\n" % \
         (license_template, rights_holder)
