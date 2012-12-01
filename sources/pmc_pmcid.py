c005a70ae18cb58ba9514cb31e052fe9a8cd7089
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2012-12-01 02:30:21 +0100

    + prevent HTTP error 414 in pmc_pmcid source

--- sources/pmc_pmcid.py
+++ sources/pmc_pmcid.py
@@ -3,7 +3,7 @@
 
 from urllib2 import urlopen, urlparse, Request, HTTPError
 from xml.etree.cElementTree import dump, ElementTree
-from os import listdir, path
+from os import listdir, path, remove
 from sys import stdin, stderr
 
 from pmc import _get_article_contrib_authors, _get_article_title, _get_article_abstract, \
@@ -22,16 +22,30 @@ def download_metadata(target_directory):
     if len(pmcids) == 0:
         raise RuntimeError, 'No PMCIDs found.'
 
-    url = _get_query_url_from_pmcids(pmcids)
-    yield { 'url': url, 'completed': 0, 'total': 1 }
+    # delete files from earlier invocations
+    listing = listdir(target_directory)
+    for filename in listing:
+        file_path = path.join(target_directory, filename)
+        stderr.write("Removing “%s” … " % file_path)
+        remove(file_path)
+        stderr.write("done.\n")
 
-    url_path = urlparse.urlsplit(url).path
-    local_filename = path.join(target_directory, \
-        url_path.split('/')[-1])
-    with open(local_filename, 'wb') as local_file:
-        content = _get_file_from_pmcids(pmcids)
-        local_file.write(content.read())
-        yield { 'url': url, 'completed': 1, 'total': 1 }
+    # chunk function by nosklo, source:
+    # <http://stackoverflow.com/questions/434287/what-is-the-most-pythonic-way-to-iterate-over-a-list-in-chunks#answer-434328>
+    def chunker(seq, size):
+        return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
+
+    for i, chunk in enumerate(chunker(pmcids, 365)):
+        url = _get_query_url_from_pmcids(chunk)
+        yield { 'url': url, 'completed': 0, 'total': 1 }
+
+        url_path = urlparse.urlsplit(url).path
+        local_filename = path.join(target_directory, \
+            url_path.split('/')[-1] + str(i))
+        with open(local_filename, 'wb') as local_file:
+            content = _get_file_from_pmcids(chunk)
+            local_file.write(content.read())
+            yield { 'url': url, 'completed': 1, 'total': 1 }
 
 
 def list_articles(target_directory, supplementary_materials=False, skip=[]):
