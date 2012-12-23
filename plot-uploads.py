42e9adf54dd6306ddd6c3d93f74cfa037436d5cc
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2012-12-23 22:16:14 +0100

    * better script for plotting uploads against time

--- plot-uploads.py
+++ plot-uploads.py
@@ -4,43 +4,56 @@
 import pylab
 
 from datetime import date
+from math import sqrt
 from helpers import mediawiki
 
 uploads = mediawiki.get_uploads()
 
 uploads_per_month = {}
 uploads_per_day = {}
-uploads_total = {}
-
-total = 0
 
 for (timestamp, title) in uploads:
     day = timestamp.date()
-    total += 1
-    uploads_total[timestamp] = total
     try:
         uploads_per_day[day] += 1
     except KeyError:
         uploads_per_day[day] = 1
-    month = date(timestamp.year, timestamp.month, 15)
-    try:
-        uploads_per_month[month] += 1
-    except KeyError:
-        uploads_per_month[month] = 1
 
-# FIXME: this is wrong
-timestamps, totalcount = zip(*sorted(uploads_total.items()))
-pylab.plot(timestamps, totalcount)
-
-months, monthcount = zip(*sorted(uploads_per_month.items()))
-pylab.plot(months, monthcount)
+uploads_total = {}
+totalcount = 0
 
 days, daycount = zip(*sorted(uploads_per_day.items()))
-pylab.scatter(days, daycount)
 
-pylab.title("Open Access Media Importer contributions")
+uploads_total_per_day = {}
+totalcount = 0
+
+for day, count in zip(days, daycount):
+    totalcount += count
+    uploads_total_per_day[day] = totalcount
+
+timestamps, totalcount = zip(*sorted(uploads_total_per_day.items()))
+pylab.plot(
+    timestamps,
+    totalcount,
+    c='black',
+    linewidth=1,
+    zorder=-1
+)
+
+dayarea = [sqrt(d)*100 for d in daycount]
+pylab.scatter(
+    x=days,
+    y=totalcount,
+    s=dayarea,
+   c=daycount,
+   cmap=pylab.cm.summer_r,
+   linewidths=1,
+   edgecolor='black'
+)
+
+pylab.title("Contributions using Open Access Media Importer")
 pylab.xlabel("Time (UTC)")
-pylab.ylabel("Uploads")
+pylab.ylabel("Edits")
 
-
+pylab.colorbar().set_ticks(range(0, max(daycount), 5))
 pylab.show()
