1da13d84fb60853bc52d0a31a75147ef60ac4cd9
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2013-07-28 04:16:12 +0200

    + proper naming for cached files, fixes bug #83

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,4 +1,7 @@
 from autovividict import autovividict
+from base64 import b64encode
+from urllib2 import urlparse
+from os import path
 
 def make_datestring(year, month, day):
     datestring = "%04d" % year  # YYYY
@@ -7,3 +10,8 @@ def make_datestring(year, month, day):
     if day is not None:
         datestring += "-%02d" % day  # YYYY-MM-DD
     return datestring
+
+def filename_from_url(url):
+    url_path = urlparse.urlsplit(url).path
+    extension = path.splitext(url_path)[1]
+    return b64encode(url) + extension
